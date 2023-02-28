d0f7c3e08f70e93bd3ef3ed296acaac706c190a2
Debytd <111027448+Debytd@users.noreply.github.com>
2023-02-28 11:58:06 -0600

    Update sucesion.py

--- sucesion.py
+++ sucesion.py
@@ -2,8 +2,8 @@ def suc(N):
   s = []
   ss = []
   for i in range(N):
-    s.append(1/10**i)
-    ss.append('\\frac\{1}{10^{' + str(i) +'}}')
+    s.append(1/10**(i+1))
+    ss.append('\\frac\{1}{10^{' + str(i+1) +'}}')
   print(ss)
   print(s)
   
